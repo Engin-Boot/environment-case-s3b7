c752936f78e2fdf4337257ea1e7f2f11971440dc
Nadimpalli <vinay.n.varma189@gmail.com>
2020-09-23 23:44:31 +0530

    reduce cyclomatic_complexity in CosoleWriter

--- Environmet-Monitoring/src/main/java/com/philips/bootcamp/sender/ConsoleWriter.java
+++ Environmet-Monitoring/src/main/java/com/philips/bootcamp/sender/ConsoleWriter.java
@@ -4,18 +4,23 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ConsoleWriter {
-    public static void writeReadingsToConsole(List<String[]> readings) {
-        for (String[] reading : readings) {
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+    public static void writeReadingsToConsole(List<String[]> readingsToBePrintedOnConsole) {
+        for (String[] reading : readingsToBePrintedOnConsole) {
+            
+            delayExecutionBySeconds(2);
+            
 			for(String element: reading){
                 System.out.print(element + ' ');
             }
             System.out.println();
 		}
-	}
+    }
+    
+    public static void delayExecutionBySeconds(int seconds){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
