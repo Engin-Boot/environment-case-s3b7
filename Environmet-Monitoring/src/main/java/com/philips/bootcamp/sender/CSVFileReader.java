9e936f06f704c85617ce203889c6247d49e9d55b
Nadimpalli <vinay.n.varma189@gmail.com>
2020-09-23 23:36:38 +0530

    added target folder to ignore

--- Environmet-Monitoring/src/main/java/com/philips/bootcamp/sender/CSVFileReader.java
+++ Environmet-Monitoring/src/main/java/com/philips/bootcamp/sender/CSVFileReader.java
@@ -9,25 +9,34 @@ import java.util.List;
 public class CSVFileReader { //TODO: implement FileReader as a interface
 
     public static List<String []> readCSVToList(String csvFileName) throws IOException { // advantages for static block?
-		List<String []>  readingList = new ArrayList<String []>();
-        String individualRow = "";
+		List<String []>  temperatureAndHumidityReadings = new ArrayList<String []>();
+        
         int numberOfRowsToSkip = 1; // TODO: move to a config file
         BufferedReader csvBufferedReader = new BufferedReader(new FileReader(csvFileName));
         
         for(int i = 0; i< numberOfRowsToSkip; i++){
             csvBufferedReader.readLine(); // this will read the first line
         }
-		while((individualRow = csvBufferedReader.readLine()) != null) { // TODO: move to a new function for better testing
+        temperatureAndHumidityReadings = getReadingsFromBufferReaderAsListOfStrings(csvBufferedReader);
+
+		csvBufferedReader.close();
+		return temperatureAndHumidityReadings;
+    }
+    
+    public static List<String []> getReadingsFromBufferReaderAsListOfStrings(BufferedReader buffer) throws IOException{
+
+        String individualRow = "";
+		List<String []>  readingList = new ArrayList<String []>();
+
+        while((individualRow = buffer.readLine()) != null) { // TODO: move to a new function for better testing
 
 			String[] rowEntries = individualRow.split(",");
 			if(rowEntries.length > 1){
 				readingList.add(rowEntries);
             }
         }
-        
-		csvBufferedReader.close();
-		return readingList;
-	}
+        return readingList;
+    }
 }
 
 //List([136,96], [55,23], [], []...)
