ebe3e4f24bf45839e84e0e1dc1094e83dbe7c6b8
Ture <PrajyotDushyant.Ture@philips.com>
2020-09-23 19:44:27 +0530

    Final

--- Environmet-Monitoring/src/main/java/com/philips/bootcamp/receiver/ReceivingTheDataFromConsole.java
+++ Environmet-Monitoring/src/main/java/com/philips/bootcamp/receiver/ReceivingTheDataFromConsole.java
@@ -2,17 +2,15 @@ package com.philips.bootcamp.receiver;
 
 public class ReceivingTheDataFromConsole {
 
-	public static void checkingReadingsOfDevice(int humiditySensedFromDevice, int temperatureSensedFromDevice) {
-        System.out.println("Temperature is "+temperatureSensedFromDevice+"  Humidity is "+humiditySensedFromDevice);
-        boolean TemperatureUnderWarningLimit=RangeChecker.checkIfTheTemperatureDoesNotViolatesWarningLevels(
-                temperatureSensedFromDevice);
-       boolean HumidityUnderWarningLimit=RangeChecker.checkIfTheHumidityDoesNotViolatesWarningLevel(humiditySensedFromDevice);
-       boolean TemperatureUnderErrorLimit=RangeChecker.checkIfTheTemperatureDoesNotViolatesErrorLevels(temperatureSensedFromDevice);
-       boolean HumidityUnderErrorLimit=RangeChecker.checkIfTheHumidityDoesNotViolatesErrorLevel(humiditySensedFromDevice);
-        boolean TemperatureInRange = TemperatureUnderWarningLimit && TemperatureUnderErrorLimit;
-        boolean HumidityinRange = HumidityUnderWarningLimit && HumidityUnderErrorLimit;
-        CheckIfAllDataUnderRequiredLimit.displayStatus(TemperatureInRange, HumidityinRange);
-        
-	}
+public static void checkingReadingsOfDevice(int humiditySensedFromDevice, int temperatureSensedFromDevice) {
+
+System.out.println("Temperature is ::"+temperatureSensedFromDevice+"  Humidity is ::"+humiditySensedFromDevice);
+boolean TemperatureInRange=RangeChecker.checkIfTheTemperatureDoesNotViolatesWarningLevels(temperatureSensedFromDevice) && 
+RangeChecker.checkIfTheTemperatureDoesNotViolatesErrorLevels(temperatureSensedFromDevice);
+boolean HumidityinRange =RangeChecker.checkIfTheHumidityDoesNotViolatesWarningLevel(humiditySensedFromDevice) && 
+RangeChecker.checkIfTheHumidityDoesNotViolatesErrorLevel(humiditySensedFromDevice);
+CheckIfAllDataUnderRequiredLimit.displayStatus(TemperatureInRange, HumidityinRange,temperatureSensedFromDevice,humiditySensedFromDevice);
+
+}
 
 }
